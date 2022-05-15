f3785684261021bb1c63900fbc5a2ec486a8b287
ProgrammerSteven <Stevenmb1995@gmail.com>
2022-05-15 18:46:52 -0500

    added resume

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import initTilt from "./tilt";
 import initSr from "./sr";
 import { addResume } from "./utils";
-import resume from "../assets/resume.pdf";
+import resume from "../assets/RESUME_5_15_2022.pdf";
 
 export default function initApp() {
   initSr();
