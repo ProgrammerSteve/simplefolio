5810b559b8a8c55627bb41ba697688ec04b99702
ProgrammerSteven <Stevenmb1995@gmail.com>
2023-04-12 23:00:21 -0500

    updated resume

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import initTilt from "./tilt";
 import initSr from "./sr";
 import { addResume } from "./utils";
-import resume from "../assets/RESUME_1_8_2023.pdf";
+import resume from "../assets/StevenMBautista.pdf";
 
 export default function initApp() {
   initSr();
